8df464bf5976878d9d05058ec9f9b83d648a89d8
Noah Laghlali <jeanmichelpichou1@gmail.com>
2024-12-12 15:24:03 +0100

    Modification Test Classe
    Ajout de methode de test pour les add de la classe Classe

--- Test/TestClasse.java
+++ Test/TestClasse.java
@@ -1,10 +1,13 @@
+import gen_diagrammes.Attribut;
 import gen_diagrammes.Classe;
+import gen_diagrammes.Methode;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -50,4 +53,74 @@ public class TestClasse {
         assertEquals("class", classe.getType());
     }
 
+    @Test
+    public void testAjout() {
+        Classe classe1 = new Classe();
+
+        ArrayList<Classe> ac = new ArrayList<>();
+        ArrayList<Attribut> aa = new ArrayList<>();
+        ArrayList<Methode> am = new ArrayList<>();
+
+        assertEquals(ac, classe1.getParents());
+        assertEquals(aa, classe1.getAttributs());
+        assertEquals(am, classe1.getMethodes());
+
+        Classe classe2 = new Classe();
+        Attribut attribut = new Attribut();
+        Methode methode = new Methode();
+
+        ac.add(classe2);
+        aa.add(attribut);
+        am.add(methode);
+
+        classe1.addParent(classe2);
+        classe1.addAttribut(attribut);
+        classe1.addMethode(methode);
+
+        assertEquals(ac, classe1.getParents());
+        assertEquals(aa, classe1.getAttributs());
+        assertEquals(am, classe1.getMethodes());
+    }
+
+
+    @Test
+    public void testAjoutPlusieursParentMemeType() {
+        Classe classe1 = new Classe();
+
+        ArrayList<Classe> ac = new ArrayList<>();
+
+        Classe classe2 = new Classe();
+        Classe classe3 = new Classe();
+
+        ac.add(classe2);
+        classe1.addParent(classe2);
+
+        assertEquals(ac, classe1.getParents());
+
+        ac.add(classe3);
+        classe1.addParent(classe3);
+
+        //Il faut faire une exeption pour ça
+        assertEquals(ac, classe1.getParents());
+    }
+
+    @Test
+    public void testAjoutPlusieursParentPasMemeType() {
+        Classe classe1 = new Classe();
+
+        ArrayList<Classe> ac = new ArrayList<>();
+
+        Classe classe2 = new Classe();
+        Classe classe3 = new Classe("test1","test2","test3");
+
+        ac.add(classe2);
+        classe1.addParent(classe2);
+
+        assertEquals(ac, classe1.getParents());
+
+        ac.add(classe3);
+        classe1.addParent(classe3);
+
+        assertEquals(ac, classe1.getParents());
+    }
 }
--- src/gen_diagrammes/Classe.java
+++ src/gen_diagrammes/Classe.java
@@ -56,6 +56,9 @@ public class Classe {
         this.nom = "Classe";
         this.acces = PUBLIC;
         this.type = CLASS;
+        this.parents = new ArrayList<Classe>();
+        this.methodes = new ArrayList<Methode>();
+        this.attributs = new ArrayList<Attribut>();
     }
 
 
