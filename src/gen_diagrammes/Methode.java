1f778ea1596792bff876bdba6905880b71b10429
tuline-leveque <tuline.leveque6@etu.univ-lorraine.fr>
2024-12-12 10:44:39 +0100

    toString et getter et setters classe Methode

--- src/gen_diagrammes/Methode.java
+++ src/gen_diagrammes/Methode.java
@@ -10,4 +10,39 @@ public class Methode {
         this.acces = acces;
         this.typeRetour = typeRetour;
     }
+
+    public Methode() {
+        this.nom = "Inconnu";
+        this.acces = "Inconnu";
+        this.typeRetour = "Inconnu";
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getAcces() {
+        return acces;
+    }
+
+    public void setAcces(String acces) {
+        this.acces = acces;
+    }
+
+    public String getTypeRetour() {
+        return typeRetour;
+    }
+
+    public void setTypeRetour(String typeRetour) {
+        this.typeRetour = typeRetour;
+    }
+
+    public String toString() {
+        String texte = "Nom : " + nom + ", acces : " + acces + ", type de retour : " + typeRetour;
+        return texte;
+    }
 }
