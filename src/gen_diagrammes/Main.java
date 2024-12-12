c18af05deb2e99dc3f19b9367c14f569e07152a8
Rémi Choffat <remi.choffat@gmail.com>
2024-12-12 15:09:52 +0100

    Chargement des fichiers .class avec chemin absolu

--- src/gen_diagrammes/Main.java
+++ src/gen_diagrammes/Main.java
@@ -21,25 +21,16 @@ public class Main {
                     d.ajouterClasse(new Classe(res));
                 } catch (Exception e) {
                     System.err.println("Error while loading class " + res);
-                    e.printStackTrace();
                 }
 
                 System.out.println(d);
             } else {
-
                 //Exporter exp = new Exporter();
                 //exp.export(d,res)
             }
             System.out.println("Enter a class path, or type *export to export diagram : ");
             res = sc.nextLine();
         }
-        /*
-        try { //pour tester si une classe affiche tout bien
-            Classe c = new Classe("gen_diagrammes.Classe");
-            System.out.println(c.toString());
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }*/
 
     }
 
