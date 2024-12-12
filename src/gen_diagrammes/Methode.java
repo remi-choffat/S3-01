b80936be90d2619903e7bc0f1637a36b1d094900
tuline-leveque <tuline.leveque6@etu.univ-lorraine.fr>
2024-12-12 10:38:40 +0100

    ajout classe Methode

---
+++ src/gen_diagrammes/Methode.java
@@ -0,0 +1,13 @@
+package gen_diagrammes;
+
+public class Methode {
+    private String nom;
+    private String acces;
+    private String typeRetour;
+
+    public Methode(String nom, String acces, String typeRetour) {
+        this.nom = nom;
+        this.acces = acces;
+        this.typeRetour = typeRetour;
+    }
+}
