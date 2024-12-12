b387a73f2ab0617a4b38409bed6191bf1df650bc
tuline-leveque <tuline.leveque6@etu.univ-lorraine.fr>
2024-12-12 10:42:20 +0100

    ajout classe Attribut

---
+++ src/gen_diagrammes/Attribut.java
@@ -0,0 +1,48 @@
+package gen_diagrammes;
+
+public class Attribut {
+    private String nom;
+    private String typeAcces;
+    private String type;
+
+    public Attribut(String nom, String typeAcces, String type) {
+        this.nom = nom;
+        this.typeAcces = typeAcces;
+        this.type = type;
+    }
+
+    public Attribut() {
+        this.nom = "Inconnu";
+        this.typeAcces = "Inconnu";
+        this.type = "Inconnu";
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getTypeAcces() {
+        return typeAcces;
+    }
+
+    public void setTypeAcces(String typeAcces) {
+        this.typeAcces = typeAcces;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String toString() {
+        String texte = "Nom : "+ nom + ", type d'acces : " + typeAcces + ", type :  " + type;
+        return texte;
+    }
+}
