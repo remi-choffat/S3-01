9df7efb47b4cbac3081425c737f5ff4a7a976e10
GabinM <gabin.mathieu54@gmail.com>
2024-12-12 11:25:56 +0100

    added main.java - theorical way to use application

--- src/gen_diagrammes/Main.java
+++ src/gen_diagrammes/Main.java
@@ -1,9 +1,41 @@
 package gen_diagrammes;
+import java.util.Scanner;
+
+//fonctionnement théorique de l'application à la fin de l'itération 1
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        //diagramme d;
+        if(args.length == 0) {
+            //d = new Diagramme(args[0]);
+        } else {
+            //d = new Diagramme();
+        }
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter a class path (absolute path if no selected package) : ");
+        String res = sc.nextLine();
+        while( ! res.equals("q")){
+            if(! res.equals("*export")){
+                /*
+                try{
+                    d.addClasse(res);
+                }catch (Exception e) {
+                    System.out.println("error whil loading class " + res);
+                    e.printStackTrace();
+                }*/
+
+                //System.out.println(d.toString());
+            } else {
+                System.out.println("enter a file path and name to export your diagram : ");
+                res = sc.nextLine();
+                //Exporter exp = new Exporter();
+                //exp.export(d,res)
+            }
+            System.out.println("enter a class path, or type *export to export diagram : ");
+            res = sc.nextLine();
+        }
+
     }
 
 }
