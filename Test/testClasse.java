1bfed71635ee1948208a7a0bbae3a5ad1e0d9390
Noah Laghlali <jeanmichelpichou1@gmail.com>
2024-12-12 11:34:41 +0100

    ajout Test
    Classe de test pour Classe

---
+++ Test/testClasse.java
@@ -0,0 +1,37 @@
+import gen_diagrammes.Classe;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class testClasse {
+
+
+    @BeforeAll
+    public static void setdown() throws SQLException, ClassNotFoundException {
+    }
+
+    @BeforeEach
+    public void setUp() throws SQLException, ClassNotFoundException {
+    }
+
+    @AfterEach
+    public void set() throws SQLException, ClassNotFoundException {
+    }
+
+    @Test
+    public void testConstructeurVide() {
+
+            Classe classe = new Classe();
+            //System.out.println(p);
+            assertEquals("Classe", classe.getNom());
+        assertEquals("public", classe.getAcces());
+        assertEquals("class", classe.getType());
+
+    }
+
+}
