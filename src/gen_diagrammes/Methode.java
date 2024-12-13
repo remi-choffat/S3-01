efe127fa0fd96623aaa4ccd08465f867be498f99
tuline-leveque <tuline.leveque6@etu.univ-lorraine.fr>
2024-12-13 08:55:35 +0100

    toString de la classe Methode modifie au format UML

--- src/gen_diagrammes/Methode.java
+++ src/gen_diagrammes/Methode.java
@@ -1,5 +1,7 @@
 package gen_diagrammes;
 
+import java.util.ArrayList;
+
 /**
  * Représente une méthode d'une classe du diagramme
  */
@@ -8,17 +10,20 @@ public class Methode {
     private String nom;
     private String acces;
     private String typeRetour;
+    private ArrayList<String> parametres;
 
-    public Methode(String nom, String acces, String typeRetour) {
+    public Methode(String nom, String acces, String typeRetour, ArrayList<String> parametres) {
         this.nom = nom;
         this.acces = acces;
         this.typeRetour = typeRetour;
+        this.parametres = parametres;
     }
 
     public Methode() {
         this.nom = "Inconnu";
         this.acces = "Inconnu";
         this.typeRetour = "Inconnu";
+        this.parametres = new ArrayList<>();
     }
 
     public String getNom() {
@@ -45,7 +50,34 @@ public class Methode {
         this.typeRetour = typeRetour;
     }
 
+    public ArrayList<String> getParametres() { return parametres; }
+
+    public void setParametres(ArrayList<String> parametres) { this.parametres = parametres; }
+
+
     public String toString() {
-        return "Nom : " + nom + ", accès : " + acces + ", type de retour : " + typeRetour;
+        String resultat = "";
+        String texteAcces = "";
+        switch (this.acces) {
+            case Classe.PRIVATE:
+                texteAcces = "-";
+                break;
+            case Classe.PUBLIC:
+                texteAcces = "+";
+                break;
+            case Classe.PROTECTED:
+                texteAcces = "#";
+                break;
+            default:
+                texteAcces = "~";
+                break;
+        }
+        resultat = texteAcces + " " + nom + "(";
+        for (String parametre : parametres) {
+            resultat += parametre + ", ";
+        }
+        resultat = resultat.substring(0, resultat.length() - 2);
+        resultat = resultat + ") : " + typeRetour;
+        return resultat;
     }
 }
